1fedb9a2c3e834bcaf25b76a03126e61d9d6d4df
ferrcol <colcombetfernando@gmail.com>
2023-04-01 18:35:22 +0200

    remove files with sensitive data

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cy-spotify-tests
-test in spotify
+Tests in Spotify
+
+You must have a user on Spotify and replace the user and pass in the next command:
+
+npx cypress open --env user=USERNAME,pass=PASSWORD
--- support/index.js
+++ support/index.js
@@ -1,3 +1,11 @@
+const origLog = Cypress.log;
+Cypress.log = function (opts, ...other) {
+  if (opts.displayName === 'script' || opts.name === 'request') {
+    return;
+  }
+  return origLog(opts, ...other);
+  
+};
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false
 })
