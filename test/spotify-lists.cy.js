f541343d811fa8561b747402aea3f3c38893b30c
ferrcol <colcombetfernando@gmail.com>
2023-04-11 16:49:02 +0200

    format the files

--- test/spotify-lists.cy.js
+++ test/spotify-lists.cy.js
@@ -1,27 +1,27 @@
-import { LoginPage } from "../cypress/page-objects/LoginPage"
-import { LandingPage } from "../cypress/page-objects/LandingPage"
+import { LoginPage } from "../cypress/page-objects/LoginPage";
+import { LandingPage } from "../cypress/page-objects/LandingPage";
 
-const user = Cypress.env('user');  
-const pass = Cypress.env('pass');
+const user = Cypress.env("user");
+const pass = Cypress.env("pass");
 
-describe('list', () => {
+describe("list", () => {
   const loginpage = new LoginPage();
   const landingpage = new LandingPage();
-  const newListName = 'My Favorite Songs' + Date. now();
+  const newListName = "My Favorite Songs" + Date.now();
 
   before(() => {
     landingpage.goToLoginPage();
-    loginpage.dologin(user,pass);
-    landingpage.shouldHaveUser('ferrcol')
+    loginpage.dologin(user, pass);
+    landingpage.shouldHaveUser("ferrcol");
   });
 
-  it('should create list successfully', () => {
+  it("should create list successfully", () => {
     landingpage.createList(newListName);
     landingpage.shouldHaveNewList(newListName);
   });
 
-  it('should delete list successfully', () => {
+  it("should delete list successfully", () => {
     landingpage.deleteList();
     landingpage.shouldNotNewList(newListName);
-  })  
-})
+  });
+});
