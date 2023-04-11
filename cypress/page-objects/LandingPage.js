b73ad37a5149ac3244c67fcc326d325c3a3e2d29
ferrcol <colcombetfernando@gmail.com>
2023-04-11 16:49:02 +0200

    format the files

--- cypress/page-objects/LandingPage.js
+++ cypress/page-objects/LandingPage.js
@@ -1,36 +1,38 @@
 export class LandingPage {
-    navigate() {
-        cy.visit('/us/');
-    }
+  navigate() {
+    cy.visit("/us/");
+  }
 
-    shouldHaveUser(username) {
-        cy.get('[data-testid="user-widget-name"]').should('have.text',username);
-    }
+  shouldHaveUser(username) {
+    cy.get('[data-testid="user-widget-name"]').should("have.text", username);
+  }
 
-    goToLoginPage() {
-        cy.visit('/');
-        cy.get('#onetrust-accept-btn-handler').click()
-        cy.get('button[data-testid="login-button"]').click();
-    }
+  goToLoginPage() {
+    cy.visit("/");
+    cy.get("#onetrust-accept-btn-handler").click();
+    cy.get('button[data-testid="login-button"]').click();
+  }
 
-    createList(ListName) {
-        cy.get('button[data-testid="create-playlist-button"]').click();
-        cy.get('button[aria-label$="– Edit details"]').click();
-        cy.get('input[data-testid="playlist-edit-details-name-input"]').type(ListName);
-        cy.get('button[data-testid="playlist-edit-details-save-button"]').click();
-    }
+  createList(ListName) {
+    cy.get('button[data-testid="create-playlist-button"]').click();
+    cy.get('button[aria-label$="– Edit details"]').click();
+    cy.get('input[data-testid="playlist-edit-details-name-input"]').type(ListName);
+    cy.get('button[data-testid="playlist-edit-details-save-button"]').click();
+  }
 
-    shouldHaveNewList(ListName) {
-        cy.get('[data-testid="rootlist"] li span').first().should('have.text',ListName);
-    }
+  shouldHaveNewList(ListName) {
+    cy.get('[data-testid="rootlist"] li span')
+      .first()
+      .should("have.text", ListName);
+  }
 
-    deleteList() {
-        cy.get('[data-testid="rootlist-item"]').first().rightclick();
-        cy.get('#context-menu button').eq(4).click();
-        cy.get('button[aria-label^="Delete "]').click();
-    }
+  deleteList() {
+    cy.get('[data-testid="rootlist-item"]').first().rightclick();
+    cy.get("#context-menu button").eq(4).click();
+    cy.get('button[aria-label^="Delete "]').click();
+  }
 
-    shouldNotNewList(ListName) {
-        cy.get('[data-testid="rootlist"] li span').should('not.include.text',ListName);
-    }
+  shouldNotNewList(ListName) {
+    cy.get('[data-testid="rootlist"] li span').should("not.include.text",ListName);
+  }
 }
--- test/spotify-login.cy.js
+++ test/spotify-login.cy.js
@@ -1,45 +1,47 @@
+import { LoginPage } from "../cypress/page-objects/LoginPage";
+import { ResetPage } from "../cypress/page-objects/ResetPage";
+import { LandingPage } from "../cypress/page-objects/LandingPage";
 
-import { LoginPage } from "../cypress/page-objects/LoginPage"
-import { ResetPage } from "../cypress/page-objects/ResetPage"
-import { LandingPage } from "../cypress/page-objects/LandingPage"
+const user = Cypress.env("user");
+const pass = Cypress.env("pass");
 
-const user = Cypress.env('user') 
-const pass = Cypress.env('pass')  
+describe("login",
+  {
+    testIsolation: true,
+  },
+  () => {
+    const loginpage = new LoginPage();
+    const resetpage = new ResetPage();
+    const landingpage = new LandingPage();
 
-describe('login',  {
-  testIsolation: true,
-},() => {
-  const loginpage = new LoginPage()
-  const resetpage = new ResetPage()
-  const landingpage = new LandingPage()
+    it("should login successfully", () => {
+      landingpage.goToLoginPage();
+      loginpage.dologin(user, pass);
+      landingpage.shouldHaveUser("ferrcol");
+    });
 
-  it('should login successfully', () => {
-    landingpage.goToLoginPage();
-    loginpage.dologin(user,pass);
-    landingpage.shouldHaveUser('ferrcol')
-  })
+    it("should show an error for wrong user", () => {
+      landingpage.goToLoginPage();
+      loginpage.dologin("aaa@gmail.com", pass);
+      loginpage.shouldHaveErrorMsg("Error");
+    });
 
-  it('should show an error for wrong user', () => {
-    landingpage.goToLoginPage();
-    loginpage.dologin('aaa@gmail.com',pass);
-    loginpage.shouldHaveErrorMsg('Error')
-  })
+    it("should show an error for wrong pass", () => {
+      landingpage.goToLoginPage();
+      loginpage.dologin(user, "as!");
+      loginpage.shouldHaveErrorMsg("Error");
+    });
 
-  it('should show an error for wrong pass', () => {
-    landingpage.goToLoginPage();
-    loginpage.dologin(user,'as!');
-    loginpage.shouldHaveErrorMsg('Error')
-  })
+    it("should show an error for empty fields", () => {
+      landingpage.goToLoginPage();
+      loginpage.dologin(user, "as!");
+      loginpage.shouldHaveErrorMsg("Error");
+    });
 
-  it('should show an error for empty fields', () => {
-    landingpage.goToLoginPage();
-    loginpage.dologin(user,'as!');
-    loginpage.shouldHaveErrorMsg('Error')    
-  })
-
-  it('should show a message for successful password reset', () => {
-    resetpage.navigate();
-    resetpage.doResetPass('bla');
-    resetpage.shouldHaveResetMsg('Password Reset')
-  })
-})
+    it("should show a message for successful password reset", () => {
+      resetpage.navigate();
+      resetpage.doResetPass("bla");
+      resetpage.shouldHaveResetMsg("Password Reset");
+    });
+  }
+);
